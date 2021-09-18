c556dcff047c9c67a228646bbcde26197717f540
Graiden234 <90862822+Graiden234@users.noreply.github.com>
2021-09-18 11:52:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import discord
 from discord.ext import commands
-Bot = commands.Bot(command_prefix="!mm ")
+intentsa = discord.Intents(message=True, guilds=True, reactions=True, members=True, presences=True)
+Bot = commands.Bot(command_prefix="!mm ", intentsa=intentsa)
+
+Token = open("token.txt", "r").read()
 
 
 @Bot.event
 async def on_ready():
     print("Ben hazırım")
 
+
 @Bot.command()
-async def Animot(msg):
-    await msg.send("Bu bir bottur, yapımcısı Graiden'dır.")
-Bot.run("ODg3NzQ0NTIyNTkzOTE4OTc4.YUImgw.tuFeGkNCrA1M0sYeS8KUnfKtZUI")
+async def animot(msg):
+    await msg.send("Ben sayın Animot, daha yeni yazılmaya başlanmış bir botum pek özelliğim yok.")
+
+
+@Bot.event
+async def on_member_join(member):
+    print(f"{member} aramıza katıldı, kurallara uy lütfen!!")
+
+
+@Bot.command()
+async def bö(msa):
+    await msa.send("https://i.pinimg.com/736x/1e/3e/62/1e3e62961cde08c1ee439ea017715583.jpg")
+
+
+@Bot.event
+async def on_member_remove(member):
+    print(f"{member} aramızdan ayrıldı, civcivin karizmasına dayanamadı!!")
+
+
+Bot.run("Token")
